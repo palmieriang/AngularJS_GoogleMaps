d7439585a80f753cf821e685fea14ace0780a367
Angelo Palmieri <palmieri.ang@gmail.com>
2017-10-02 16:01:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+angular.module('myModule', [])
+.controller('mapsController', function($scope) {
+
+	function initMap() {
+	  // Create a map object and specify the DOM element for display.
+	  var map = new google.maps.Map(document.getElementById('map'), {
+	    center: {lat: -34.397, lng: 150.644},
+	    zoom: 8
+	  });
+	}
+
+	initMap();
+
+});
