0b2b56705f849a7cfb88701a2971230e10437166
Angelo Palmieri <palmieri.ang@gmail.com>
2017-10-03 17:56:40 +0100

    form input

--- script.js
+++ script.js
@@ -46,6 +46,59 @@ angular.module('myModule', ['ngMaterial'])
 
   initializeRequestForm();
 
+  function validate(form)
+  {
+    var valid = true;
+    form.firstName.error = '';
+    form.lastName.error = '';
+    form.address1.error = '';
+    form.address2.error = '';
+    form.postcode.error = '';
+    // form.signDate.error = '';
+
+    if(form.firstName.value == null || form.firstName.value.length < 2 || form.firstName.value.length > 100)
+    {
+      form.firstName.error = 'First name must be between 2 and 100 characters long';
+      valid = false;
+    }
+
+    if(form.lastName.value == null || form.lastName.value.length < 2 || form.lastName.value.length > 100)
+    {
+      form.lastName.error = 'Last name must be between 2 and 100 characters long';
+      valid = false;
+    }
+
+    if(form.address1.value.length < 2 || form.address1.value.length > 100)
+    {
+      form.address1.error = 'Address line 1 must be between 2 and 100 characters long';
+      valid = false;
+    }
+
+    if(!validatePostcode(form.postcode.value))
+    {
+      form.postcode.error = 'Invalid postcode';
+      valid = false;
+    }
+
+    // if(!form.signDate.value)
+    // {
+    //   form.signDate.error = 'You must enter a valid date';
+    //   valid = false;
+    // }
+
+    return valid;
+  }
+
+  function validatePostcode(postcode)
+  {
+    var re = /^(GIR[ ]?0AA|((AB|AL|B|BA|BB|BD|BH|BL|BN|BR|BS|BT|CA|CB|CF|CH|CM|CO|CR|CT|CV|CW|DA|DD|DE|DG|DH|DL|DN|DT|DY|E|EC|EH|EN|EX|FK|FY|G|GL|GY|GU|HA|HD|HG|HP|HR|HS|HU|HX|IG|IM|IP|IV|JE|KA|KT|KW|KY|L|LA|LD|LE|LL|LN|LS|LU|M|ME|MK|ML|N|NE|NG|NN|NP|NR|NW|OL|OX|PA|PE|PH|PL|PO|PR|RG|RH|RM|S|SA|SE|SG|SK|SL|SM|SN|SO|SP|SR|SS|ST|SW|SY|TA|TD|TF|TN|TQ|TR|TS|TW|UB|W|WA|WC|WD|WF|WN|WR|WS|WV|YO|ZE)(\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}))|BFPO[ ]?\d{1,4})$/;
+    return re.test(postcode);
+  }
+
+  $scope.addItem = function() {
+  	console.log($scope.form);
+  }
+
 	function initMap() {
 	  // Create a map object and specify the DOM element for display.
 	  var map = new google.maps.Map(document.getElementById('map'), {
