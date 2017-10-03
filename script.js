4a61cfddffaddcfa799b599b1118ab6f8c0afbcf
Angelo Palmieri <palmieri.ang@gmail.com>
2017-10-03 16:59:33 +0100

    form input

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-angular.module('myModule', [])
+angular.module('myModule', ['ngMaterial'])
 .controller('mapsController', function($scope) {
+
+  function initializeRequestForm() {
+    $scope.form = {
+      firstName : {
+        value : '',
+        error : ''
+      },
+      lastName : {
+        value : '',
+        error : ''
+      },
+      address1 : {
+        value : '',
+        error : ''
+      },
+      address2 : {
+        value : '',
+        error : ''
+      },
+      city : {
+        value : '',
+        error : ''
+      },
+      country : {
+        value : 'United Kingdom',
+        error : ''
+      },
+      postcode : {
+        value : '',
+        error : ''
+      },
+      latitude : '',
+      longitude : '',
+      quantity : {
+        value : '',
+        error : ''
+      }
+      // signDate : {
+      //     value : '',
+      //     error : ''
+      // },
+    };  
+  }
+
+  initializeRequestForm();
 
 	function initMap() {
 	  // Create a map object and specify the DOM element for display.
 	  var map = new google.maps.Map(document.getElementById('map'), {
-	    center: {lat: -34.397, lng: 150.644},
+	    center: {lat: 51.509865, lng: -0.118092},
 	    zoom: 8
 	  });
 	}
